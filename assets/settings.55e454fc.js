21103e727e9f33ceeb26a1ca056baf2edd506c90
sharmaankit25 <sharmaankit25@users.noreply.github.com>
2022-11-18 03:33:44 +0000

    deploy: dd9a7c7999f04604dec3d71a49043f0c55d37c5d

---
+++ assets/settings.55e454fc.js
@@ -0,0 +1,1 @@
+import{m as t,n as s,p as e}from"./index.73c3ee54.js";const r=t("settings",()=>({settings:s(e)}));export{r as u};
--- assets/settings.dffc955f.js
+++
@@ -1,1 +0,0 @@
-import{m as t,n as s,p as e}from"./index.2e8b88c4.js";const r=t("settings",()=>({settings:s(e)}));export{r as u};
